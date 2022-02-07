a4822b902af67e5932850c1fcd24597d5822f14c
kong92 <hongclaudia9@gmail.com>
2022-02-07 11:03:56 +0000

    Config database with environment variables

--- settings.py
+++ settings.py
@@ -7,9 +7,9 @@ from dotenv import load_dotenv
 
 load_dotenv('.env.local')
 
-RUN_ON_REAL_SERVER = os.environ.get('RUN_ON_REAL_SERVER', 'false')
-
 BASE_DIR = Path(__file__).resolve().parent
+DB_CONNECTION_URL = os.environ.get('DB_CONNECTION_URL')
+RUN_ON_REAL_SERVER = os.environ.get('RUN_ON_REAL_SERVER', 'false')
 
 DEBUG = RUN_ON_REAL_SERVER != 'true'
 
@@ -61,10 +61,7 @@ TEMPLATES = [
 ]
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.parse(DB_CONNECTION_URL),
 }
 
 AUTH_PASSWORD_VALIDATORS = [{
