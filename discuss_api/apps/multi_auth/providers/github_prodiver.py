1d1bcdc8d8c0f51a9b97853772970ba954eef39d
hseo <sh265601@gmail.com>
2023-01-03 16:21:07 +0900

    깃허브 로그인 연동 기능 추가

--- discuss_api/apps/multi_auth/providers/github_prodiver.py
+++ discuss_api/apps/multi_auth/providers/github_prodiver.py
@@ -1,4 +1,25 @@
+import uuid
 
+from django.db import transaction
+
+from discuss_api.apps.member.models import User
+from discuss_api.apps.multi_auth.models import OAuth
+import requests
 
 def exchange_token(tokens):
-    pass
+    access_token = 'token ' + tokens['access_token']
+    url = 'https://api.github.com/user'
+    headers = {"Authorization": access_token}
+
+    resp = requests.get(url=url, headers=headers)
+
+    result = resp.json()
+
+    with transaction.atomic():
+        try:
+            auth = OAuth.objects.get(provider='github', id_on_provider=result['id'])
+        except OAuth.DoesNotExist:
+            user = User.objects.create(email=result['email'], username=uuid.uuid4())
+            auth = OAuth.objects.create(provider='github', id_on_provider=result['id'], user=user)
+
+    return auth
