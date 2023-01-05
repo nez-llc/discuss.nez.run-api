6053e8d40e856f7a9e56baaa1aa20441abc7dbe2
hseo <sh265601@gmail.com>
2023-01-06 03:53:13 +0900

    페이스북 로그인 연동 (메일 수집 아직 안됨)

--- discuss_api/apps/multi_auth/providers/facebook_provider.py
+++ discuss_api/apps/multi_auth/providers/facebook_provider.py
@@ -1,5 +1,37 @@
-import pyfacebook
-
+import uuid
+import requests
+from django.db import transaction
+from discuss_api.apps.member.models import User
+from discuss_api.apps.multi_auth.models import OAuth
 
 def exchange_token(tokens):
-    pass
+    resp = requests.get(
+        'https://graph.facebook.com/v4.0/me',
+        params={
+            'fields': 'id,email',
+            'access_token': tokens['access_token'],
+        })
+
+    result = resp.json()
+
+
+    # real_resp = requests.get(
+    #     'https://graph.facebook.com/v4.0/' + result['id'],
+    #     params={
+    #         'fields': 'id,email,updated_time',
+    #         'access_token': tokens['access_token'],
+    #     })
+    #
+    # real_result = resp.json()
+    #
+    # print(real_result)
+    # print(tokens['access_token'])
+
+    with transaction.atomic():
+        try:
+            auth = OAuth.objects.get(provider='facebook', id_on_provider=result['id'])
+        except OAuth.DoesNotExist:
+            user = User.objects.create(email=uuid.uuid4().hex + '@nez.wtf', username=uuid.uuid4())
+            auth = OAuth.objects.create(provider='facebook', id_on_provider=result['id'], user=user)
+
+    return auth
